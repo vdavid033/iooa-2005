fa6e085df79c6dc6e82e0ab2a434f2e92abf526b
dominik-faks <dmilic@veleri.hr>
2025-04-28 18:30:10 +0200

    fix 3

--- frontend/quasar-project/src/pages/IndexPage.vue
+++ frontend/quasar-project/src/pages/IndexPage.vue
@@ -1,12 +1,15 @@
 <template>
-  <q-page class="q-pa-md">
-    <h1 class="text-h4 q-mb-md"File Grid</h1>
-    <file-grid
-      root-path="/"
-      class="q-mt-md"
-    />
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    >
   </q-page>
 </template>
+
 <script setup>
-import FileGrid from 'components/FileGrid';
+defineOptions({
+  name: 'IndexPage'
+});
 </script>
