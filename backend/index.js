0e1ebb765de6a6accf830a2128669613bfa80232
mmartinic <152493543+mmartinicveleri@users.noreply.github.com>
2025-05-12 17:57:17 +0200

    azuriran index.js

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,20 @@
-// API
+// Uvoz potrebnih modula
+const express = require('express');
+const bodyParser = require('body-parser'); // Za parsiranje JSON tijela
+const mysql = require('mysql2'); 
+const app = express();
+const port = process.env.PORT || 3000; // Port na kojem će server slušati
+
+// Uvoz rute (routes) za grupe
+const groupsRoute = require('./routes/groups');
+
+// Middleware za parsiranje JSON tijela
+app.use(bodyParser.json());  // Omogućava parsiranje JSON-a u tijelu zahtjeva
+
+// Postavljanje ruta
+app.use('/api/groups', groupsRoute);  
+
+// Pokretanje servera
+app.listen(port, () => {
+  console.log(`Server pokrenut na portu ${port}`);
+});
