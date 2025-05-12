040538aa18f0b2505e19e80dc633d806f1e396b8
Miletic1 <Miletic.kristijan101@gmail.com>
2025-05-12 19:25:21 +0200

    Dodane datoteke za probni login

---
+++ backend/dogadaj.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const eventsRoutes = require('./routes/events');
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/events', eventsRoutes);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Backend server radi na http://localhost:${PORT}`);
+});
