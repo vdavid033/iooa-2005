b9f57cefb299cfbbaf7e51295b23c78b90a90830
dominik-faks <dmilic@veleri.hr>
2025-06-02 11:08:46 +0200

    refactor document upload vue komponent + backend fixes

--- backend/authJwt.js
+++ backend/authJwt.js
@@ -2,58 +2,63 @@ const jwt = require("jsonwebtoken");
 const config = require("./auth_config.js");
 
 verifyTokenAdmin = (req, res, next) => {
-  /*   let token = req.headers["x-access-token"]; */
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
+    /*   let token = req.headers["x-access-token"]; */
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
 
-  if (!token) {
-    return res.status(403).send({
-      message: "No token provided!",
+    if (!token) {
+        return res.status(403).send({
+            message: "No token provided!",
+        });
+    }
+
+    jwt.verify(token, config.secret, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({
+                message: "Unauthorized!",
+            });
+        }
+        req.user = decoded;
+        /*     req.role = decoded.uloga; // Assign the role to the request object */
+        if (decoded.uloga === "admin") {
+            next();
+        } else {
+            res.status(403).send({
+                message: "Require Admin Role!",
+            });
+        }
     });
-  }
-
-  jwt.verify(token, config.secret, (err, decoded) => {
-    if (err) {
-      return res.status(401).send({
-        message: "Unauthorized!",
-      });
-    }
-    req.userId = decoded.id;
-    /*     req.role = decoded.uloga; // Assign the role to the request object */
-    if (decoded.uloga === "admin") {
-      next();
-    } else {
-      res.status(403).send({
-        message: "Require Admin Role!",
-      });
-    }
-  });
 };
 
 verifyTokenUser = (req, res, next) => {
-  /*   let token = req.headers["x-access-token"]; */
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
+    /*   let token = req.headers["x-access-token"]; */
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
 
-  if (!token) {
-    return res.status(403).send({
-      message: "No token provided!",
-    });
-  }
-
-  jwt.verify(token, config.secret, (err, decoded) => {
-    if (err) {
-      return res.status(401).send({
-        message: "Unauthorized!",
-      });
+    if (!token) {
+        return res.status(403).send({
+            message: "No token provided!",
+        });
     }
-    req.userId = decoded.id;
-  });
-  next();
+
+    jwt.verify(token, config.secret, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({
+                message: "Unauthorized!",
+            });
+        }
+
+        if (decoded.uloga !== "admin") {
+            return res.status(403).send({message: "Require Admin Role!"});
+        }
+
+        req.user = decoded;
+        next();
+    });
 };
 
 const authJwt = {
-  verifyTokenAdmin: verifyTokenAdmin,
-  verifyTokenUser: verifyTokenUser,
+    verifyTokenAdmin: verifyTokenAdmin,
+    verifyTokenUser: verifyTokenUser,
 };
 module.exports = authJwt;
