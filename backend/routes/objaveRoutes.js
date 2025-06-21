7215ce1fca06e0ace32fd46abf8bdc3714b7626d
FaksovciUProblemu <dromic@veleri.hr>
2025-06-21 20:01:14 +0200

    Backend: Dodavanje funkcije za uredjivanje vlastitih objava

--- backend/routes/objaveRoutes.js
+++ backend/routes/objaveRoutes.js
@@ -5,13 +5,17 @@ const { verifyTokenUser } = require('../authJwt')
 
 // Sve objave
 router.get('/', controller.getAllObjave)
+
 // Filtrirane objave po tagovima
 router.get('/filtrirane', controller.getFilteredObjave)
+
 // Jedna objava po ID-u
 router.get('/:id', controller.getObjavaById)
+
 // Kreiraj novu objavu
 router.post('/', verifyTokenUser, controller.createObjava)
-// Uredi objavu – samo ako je korisnik vlasnik
+
+// NOVA RUTA - Uredi objavu (samo vlastitu)
 router.put('/:id', verifyTokenUser, controller.updateObjava)
 
 module.exports = router
